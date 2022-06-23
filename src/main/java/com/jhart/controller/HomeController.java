f716226e42324e64dd51ff1bdec21d4e9c72e6c0
John Hart <jhart.naples@gmail.com>
2022-06-23 14:52:38 -0400

    updated HomeController to return enhanced date time message

--- src/main/java/com/jhart/controller/HomeController.java
+++ src/main/java/com/jhart/controller/HomeController.java
@@ -1,14 +1,31 @@
 package com.jhart.controller;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.TimeZone;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-	
+
 	@RequestMapping("/")
 	public String home() {
-		return "Hello World! " + System.currentTimeMillis();
+		return "Hello World! Current local time is: " + getTime(); 
+	}
+	
+	private String getTime() {
+		String date = LocalDate.now().toString();
+		String time = LocalTime.now().toString();
+		System.out.println("Current local date (time): " + date + " (" + time + ")");
+		String zoneId = getZoneId();
+		return date + " (" + time + ") " + "TZ: " + zoneId;
+	}
+	
+	private String getZoneId() {
+		TimeZone tz = TimeZone.getDefault();
+		return tz.getID();
 	}
 
 }
