826e8ed1435f8f4c9c80d61698f786ccb1e65e43
John Hart <jhart.naples@gmail.com>
2022-07-11 21:35:26 -0400

    main frontend welcome integration

--- frontend/heroku1/src/app/app.component.ts
+++ frontend/heroku1/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'heroku1';
+  //title = 'heroku1';
+  //message = 'Big howdy from John Hart';
+  //msg = 'XYZ';
 }
