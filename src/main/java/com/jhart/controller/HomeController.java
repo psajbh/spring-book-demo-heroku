7c8ddba62b92d3390a69bfbdc250eb860de46b50
John Hart <jhart.naples@gmail.com>
2022-07-27 16:38:37 -0400

    main - enhanced quote presentation

--- src/main/java/com/jhart/controller/HomeController.java
+++ src/main/java/com/jhart/controller/HomeController.java
@@ -22,6 +22,25 @@ public class HomeController {
 		this.quotesService = quotesService;
 	}
 	
+//	@RequestMapping("/")
+//	public String home() {
+//		return "Hello World! Current local time is: " + getTime(); 
+//	}
+//	
+//	private String getTime() {
+//		String date = LocalDate.now().toString();
+//		String time = LocalTime.now().toString();
+//		System.out.println("Current local date (time): " + date + " (" + time + ")");
+//		String zoneId = getZoneId();
+//		return date + " (" + time + ") " + "TZ: " + zoneId;
+//	}
+//	
+//	private String getZoneId() {
+//		TimeZone tz = TimeZone.getDefault();
+//		return tz.getID();
+//	}
+
+	
 	@GetMapping({"/",""})
 	public String setup(Model model) {
 		model.addAttribute("url", "/quote");
