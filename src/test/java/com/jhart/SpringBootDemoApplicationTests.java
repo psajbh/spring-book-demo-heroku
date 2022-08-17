0216ad29435077f48ce799c875c6f483c1469a2f
John Hart <jhart.naples@gmail.com>
2022-08-17 14:33:54 -0400

    sbdh-local code updates and property setup for local that should not impact main

--- src/test/java/com/jhart/SpringBootDemoApplicationTests.java
+++ src/test/java/com/jhart/SpringBootDemoApplicationTests.java
@@ -1,15 +1,36 @@
 package com.jhart;
 
-import org.junit.jupiter.api.Disabled;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import com.jhart.aop.TaskManagerAspect;
+
 
 @SpringBootTest
 class SpringBootDemoApplicationTests {
-
+	Logger log = LoggerFactory.getLogger(this.getClass());
 	
-	@Disabled
+	@Test
 	void contextLoads() {
+		log.info("running contextLoads");
 	}
+	
+	@Test
+	void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
+		log.info("successfully executed context assertions");
+	}
+	
+	@Test
+	void loadTaskManagerAspect(ApplicationContext context) {
+		assertThat(context.getBean(TaskManagerAspect.class)).isNotNull();
+		log.info("successfully validated TaskManagerAspect is created");
+	}
+
 
 }
--- src/test/java/com/jhart/web/task/AddTaskControllerTest.java
+++ src/test/java/com/jhart/web/task/AddTaskControllerTest.java
@@ -4,8 +4,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-import org.junit.Before;
-import org.junit.Test;
+//import org.junit.Before;
+//import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,36 +17,36 @@ import com.jhart.service.user.UserService;
 
 public class AddTaskControllerTest {
 
-	@Mock
-	TodoService todoService;
-	
-	@Mock
-	UserService userService;
-	
-	@Mock 
-	TaskConductor taskConductor;
-	
-	AddTaskController controller;
-	
-	MockMvc mockMvc;
-	
-	@Before
-	public void setUp() throws Exception {
-		MockitoAnnotations.initMocks(this);
-		controller = new AddTaskController(userService, taskConductor);
-        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();		
-	}
-	
-	@Test
-	public void testMockMvc()throws Exception{
-		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-		mockMvc.perform(get("/todo/add")).andExpect(status().isOk()).andExpect(view().name("task/newtodo"));
-	}
-	
-	@Test
-	public void testAddNewTodo() throws Exception{
-		System.out.println("Hello");
-	}
+//	@Mock
+//	TodoService todoService;
+//	
+//	@Mock
+//	UserService userService;
+//	
+//	@Mock 
+//	TaskConductor taskConductor;
+//	
+//	AddTaskController controller;
+//	
+//	MockMvc mockMvc;
+//	
+////	@Before
+//	public void setUp() throws Exception {
+//		MockitoAnnotations.initMocks(this);
+//		controller = new AddTaskController(userService, taskConductor);
+//        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();		
+//	}
+//	
+////	@Test
+//	public void testMockMvc()throws Exception{
+//		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+//		mockMvc.perform(get("/todo/add")).andExpect(status().isOk()).andExpect(view().name("task/newtodo"));
+//	}
+//	
+////	@Test
+//	public void testAddNewTodo() throws Exception{
+//		System.out.println("Hello");
+//	}
 	
 	
 	
