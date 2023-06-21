9eab98cdae267d50ea8c472a8c81a4038c84d401
John <jhart.naples@gmail.com>
2023-06-21 15:05:07 -0400

    asus spinnerUsers integrated spinners save status to show title over every player even if player is null

--- src/main/java/com/jhart/web/games/spinners/SpinnersController.java
+++ src/main/java/com/jhart/web/games/spinners/SpinnersController.java
@@ -81,10 +81,10 @@ public class SpinnersController {
 	public String saveSpinners(Model model, SpinnersDto spinnersDto) {
 		log.info("SpinnersController : saveSpinners - start");
 		
-		if(!validate(spinnersDto)) {
-			log.warn("SpinnersController : saveNewSpinners - players validation failure");
-			return "redirect:/spinners/index";
-		}
+//		if(!validate(spinnersDto)) {
+//			log.warn("SpinnersController : saveNewSpinners - players validation failure");
+//			return "redirect:/spinners/index";
+//		}
 		
 		if(StringUtils.isEmpty(spinnersDto.getDisplayDate())) {
 			log.warn("SpinnersController : saveSpinners - failed to process, no playDate");
@@ -157,96 +157,96 @@ public class SpinnersController {
 		return "games/spinners/save";
 	}
 	
-	private boolean validate(SpinnersDto spinnersDto) {
-		Map<String, String> uniquePlayerNames = new HashMap<>(); 
-		String player1 = spinnersDto.getPlayer1();
-		if(isNotNullOrEmpty(player1)) {
-			uniquePlayerNames.put(player1, player1);
-		}
-		else {
-			log.info("SpinnersController : saveNewSpinners - Player1 is invalid");
-			return false;
-		}
-		
-		String player2 = spinnersDto.getPlayer2();
-		if(isNotNullOrEmpty(player2)) {
-			if (null == uniquePlayerNames.get(player2)) {
-				uniquePlayerNames.put(player2, player2);
-			}
-			else {
-				log.info("SpinnersController : saveNewSpinners - Player2 is invalid");
-				return false;
-			}
-		}
-		
-		String player3 = spinnersDto.getPlayer3();
-		if(isNotNullOrEmpty(player3)) {
-			if(isNotNullOrEmpty(player2)) {
-				if (null == uniquePlayerNames.get(player3)) {
-					uniquePlayerNames.put(player3, player3);
-				}
-			}
-			else {
-				log.info("SpinnersController : saveNewSpinners - Player3 is invalid");
-				return false;
-			}
-		}
-				
-		String player4 = spinnersDto.getPlayer4();
-		if(isNotNullOrEmpty(player4)) {
-			if(isNotNullOrEmpty(player3)) {
-				if (null == uniquePlayerNames.get(player4)) {
-					uniquePlayerNames.put(player4, player4);
-				}
-			}
-			else {
-				log.info("SpinnersController : saveNewSpinners - Player4 is invalid");
-				return false;
-			}
-		}
-		
-		String player5 = spinnersDto.getPlayer5();
-		if(isNotNullOrEmpty(player5)) {
-			if(isNotNullOrEmpty(player4)) {
-				if (null == uniquePlayerNames.get(player5)) {
-					uniquePlayerNames.put(player5, player5);
-				}
-			}
-			else {
-				log.info("SpinnersController : saveNewSpinners - Player5 is invalid");
-				return false;
-			}
-		}
-
-		String player6 = spinnersDto.getPlayer6();
-		if(isNotNullOrEmpty(player6)) {
-			if(isNotNullOrEmpty(player5)) {
-				if (null == uniquePlayerNames.get(player6)) {
-					uniquePlayerNames.put(player6, player6);
-				}
-			}
-			else {
-				log.info("SpinnersController : saveNewSpinners - Player6 is invalid");
-				return false;
-			}
-		}
-
-		String player7 = spinnersDto.getPlayer7();
-		if(isNotNullOrEmpty(player7)) {
-			if (isNotNullOrEmpty(player6)) {
-				if (null == uniquePlayerNames.get(player7)) {
-					uniquePlayerNames.put(player7, player7);
-				}
-			}
-			else {
-				log.info("SpinnersController : saveNewSpinners - Player7 is invalid");
-				return false;
-			}
-		}
-		
-		return true;
-		
-	}
+//	private boolean validate(SpinnersDto spinnersDto) {
+//		Map<String, String> uniquePlayerNames = new HashMap<>(); 
+//		String player1 = spinnersDto.getPlayer1();
+//		if(isNotNullOrEmpty(player1)) {
+//			uniquePlayerNames.put(player1, player1);
+//		}
+//		else {
+//			log.info("SpinnersController : saveNewSpinners - Player1 is invalid");
+//			return false;
+//		}
+//		
+//		String player2 = spinnersDto.getPlayer2();
+//		if(isNotNullOrEmpty(player2)) {
+//			if (null == uniquePlayerNames.get(player2)) {
+//				uniquePlayerNames.put(player2, player2);
+//			}
+//			else {
+//				log.info("SpinnersController : saveNewSpinners - Player2 is invalid");
+//				return false;
+//			}
+//		}
+//		
+//		String player3 = spinnersDto.getPlayer3();
+//		if(isNotNullOrEmpty(player3)) {
+//			if(isNotNullOrEmpty(player2)) {
+//				if (null == uniquePlayerNames.get(player3)) {
+//					uniquePlayerNames.put(player3, player3);
+//				}
+//			}
+//			else {
+//				log.info("SpinnersController : saveNewSpinners - Player3 is invalid");
+//				return false;
+//			}
+//		}
+//				
+//		String player4 = spinnersDto.getPlayer4();
+//		if(isNotNullOrEmpty(player4)) {
+//			if(isNotNullOrEmpty(player3)) {
+//				if (null == uniquePlayerNames.get(player4)) {
+//					uniquePlayerNames.put(player4, player4);
+//				}
+//			}
+//			else {
+//				log.info("SpinnersController : saveNewSpinners - Player4 is invalid");
+//				return false;
+//			}
+//		}
+//		
+//		String player5 = spinnersDto.getPlayer5();
+//		if(isNotNullOrEmpty(player5)) {
+//			if(isNotNullOrEmpty(player4)) {
+//				if (null == uniquePlayerNames.get(player5)) {
+//					uniquePlayerNames.put(player5, player5);
+//				}
+//			}
+//			else {
+//				log.info("SpinnersController : saveNewSpinners - Player5 is invalid");
+//				return false;
+//			}
+//		}
+//
+//		String player6 = spinnersDto.getPlayer6();
+//		if(isNotNullOrEmpty(player6)) {
+//			if(isNotNullOrEmpty(player5)) {
+//				if (null == uniquePlayerNames.get(player6)) {
+//					uniquePlayerNames.put(player6, player6);
+//				}
+//			}
+//			else {
+//				log.info("SpinnersController : saveNewSpinners - Player6 is invalid");
+//				return false;
+//			}
+//		}
+//
+//		String player7 = spinnersDto.getPlayer7();
+//		if(isNotNullOrEmpty(player7)) {
+//			if (isNotNullOrEmpty(player6)) {
+//				if (null == uniquePlayerNames.get(player7)) {
+//					uniquePlayerNames.put(player7, player7);
+//				}
+//			}
+//			else {
+//				log.info("SpinnersController : saveNewSpinners - Player7 is invalid");
+//				return false;
+//			}
+//		}
+//		
+//		return true;
+//		
+//	}
 	
 	private boolean isNotNullOrEmpty(String string) {
 		if (string != null && !string.isEmpty()) {
