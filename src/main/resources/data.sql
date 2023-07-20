69c82affe06c608470ffe0e19977d1bfc3b3b8b5
John <jhart.naples@gmail.com>
2023-07-20 16:30:45 -0400

    asus h2_integration added back to pom sb-starter-security and added h2 database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO user (id, date_created, email, first_name, last_name, name) 
+	VALUES (-1, '2023-07-20 12:30:01','jhart.naples@gmail.com', 'John', 'Hart', 'John');
+INSERT INTO user (id, date_created, email, first_name, last_name, name) 
+	VALUES (-2, '2023-07-20 12:30:06','mays@gmail.com', 'Willy', 'Mays', 'Willy');
+INSERT INTO user (id, date_created, email, first_name, last_name, name) 
+	VALUES (-3, '2023-07-20 12:30:12', 'orlando@gmail.com', 'Orlando', 'Cepeda', 'Orlando');
+INSERT INTO user (id, date_created, email, first_name, last_name, name) 
+	VALUES (-4, '2023-07-20 12:30:20', 'fuentes@gmail.com', 'Tito', 'Fuentes', 'Tito');
