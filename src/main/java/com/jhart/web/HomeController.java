fb0cd09cbfd7bde9b308754a04d31bde37151f74
John Hart <jhart.naples@gmail.com>
2024-02-05 16:18:19 -0500

    asus main removed log,debug on HomeController

--- src/main/java/com/jhart/web/HomeController.java
+++ src/main/java/com/jhart/web/HomeController.java
@@ -11,7 +11,7 @@ public class HomeController {
 	
 	@GetMapping({"", "/", "/home"})
 	public String home() {
-		log.debug("this is a test");
+		//log.debug("this is a test");
 		return "home";
 	}
 
